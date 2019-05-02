f31079a011969b301b47cd95596bdea229f10bbd
ericjosprice <epriceutexas@gmail.com>
2019-05-02 07:00:33 -0500

    setup models, routes, pages, and a few components, namely nav,footer, and saved. dubbugged for a while. then realised i needed to run create react app, which solved my issues. committing now before i restructure the application folder

---
+++ client/src/utils/API.js
@@ -0,0 +1,9 @@
+// import axios from "axios";
+
+// // The getBooks method retrieves recipes from the server
+// // It accepts a "query" or term to search the books api for
+// export default {
+//   getRecipes: function(query) {
+//     return axios.get("/api/books", { params: { q: query } });
+//   }
+// };
